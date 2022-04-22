f14616fd87563d2fcc67f6c0bd9c4a7672e99a44
SirRiuz <50774732+SirRiuz@users.noreply.github.com>
2022-04-21 19:18:01 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ import boto3
 client = boto3.client(
     'dynamodb',
     region_name='us-east-1',
-    aws_access_key_id='AKIARJCBZXL7GZM22FGQ',
-    aws_secret_access_key='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'
+    aws_access_key_id='xxxxxxxxxxxxxxxx',
+    aws_secret_access_key='xxxxxxxxxxxxxxxx'
 )
 
 
